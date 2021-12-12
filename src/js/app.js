9874076ca3f3756d4657c00aaa675aa51179ce3b
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2021-12-12 12:19:07 -0300

    open image and draw canvas

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,27 @@
-console.log("test")
+document.querySelector('#imageFile').addEventListener('change', e => {
+  open(e);
+});
+function open(e) {
+  if (e.target.files[0]) {
+    image = e.target.files[0];
+    let url = window.URL || window.webkitURL;
+    let img = new Image();
+    img.src = url.createObjectURL(image);
+    img.onload = function () {
+      originalImg = this;
+      draw(img);
+    };
+  }
+}
+
+function draw(img) {
+  canvas = document.createElement('canvas');
+  canvas.width = img.width;
+  canvas.height = img.height;
+  c = canvas.getContext('2d');
+  c.drawImage(img, 0, 0);
+  let oldCanvas = document.getElementsByTagName('canvas')[0];
+  if (oldCanvas)
+    oldCanvas.remove();
+  document.querySelector('#workspace').appendChild(canvas);
+}
